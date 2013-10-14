4f8c4123b71ec4be9a3159a61af8b6dfb21499d8
Paul Blouët <paul@paulblouet.fr>
2013-10-14 23:42:25 +0200

    builds correctly

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,17 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char **argv){
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-	SDL_Quit();
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+ 
+  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  if (win == nullptr){
+    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
 
-	return 0;
+  SDL_Quit();
+  return 0;
 }
