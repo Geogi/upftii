e93f2afba817f6f06627fe5a2f7188d9e2f20d5e
Paul Blouët <paul@paulblouet.fr>
2013-10-15 05:00:19 +0200

    adds a working png to texture using ImageMagick

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <png.h>
+#include <wand/MagickWand.h>
 
 /* This program is currently a test, largely helped by twinklebear.github.io's excellent tutorial
  * so thanks to him/her */
@@ -10,23 +10,23 @@ int main(int argc, char **argv){
   SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 
 					 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   
-  //try to read from pngs
-  png_image *img = (png_image*) malloc(sizeof(png_image));
-  memset(img, 0, sizeof(png_image));
-  png_image_begin_read_from_file(img, "res/left.png");
-  img->format = PNG_FORMAT_RGB;
-  void *buffer = malloc(300000);
-  png_image_finish_read(img, NULL, buffer, 1, NULL);
-  free(img);
-  //end
+  MagickWandGenesis();
+  MagickWand *wand = NewMagickWand();
+  MagickReadImage(wand, "res/left.png");
+  MagickSetImageFormat(wand, "BMP");
+  size_t buffer_size;
+  unsigned char *buffer = MagickGetImageBlob(wand, &buffer_size);
 
-  SDL_RWops *stream = SDL_RWFromMem(buffer, 300000);
+  SDL_RWops *stream = SDL_RWFromMem(buffer, buffer_size);
   SDL_Surface *sur = SDL_LoadBMP_RW(stream, 1);
   if(sur == nullptr) {
     std::cout << SDL_GetError() << std::endl;
     return 1;
   }
-  free(buffer);
+  MagickRelinquishMemory(buffer);
+  DestroyMagickWand(wand);
+  MagickWandTerminus();
+
   SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, sur);
   SDL_FreeSurface(sur);
 
