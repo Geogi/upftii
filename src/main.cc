828b278ab098568f0a6fa76a4b1016a578d7c727
Paul Blouët <paul@paulblouet.fr>
2013-10-15 02:44:29 +0200

    all steps, need to uncompress pngs

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+/* This program is currently a test, largely helped by twinklebear.github.io's excellent tutorial
+ * so thanks to him/her */
 int main(int argc, char **argv){
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
- 
-  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-  if (win == nullptr){
-    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 1280, 768, SDL_WINDOW_SHOWN);
+  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 
+					 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  
+  SDL_Surface *sur = SDL_CreateRGBSurface(0, 200, 500, 32, 0, 0, 0, 0);
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, sur);
+  SDL_FreeSurface(sur);
+
+  SDL_RenderClear(ren);
+  SDL_RenderCopy(ren, tex, NULL, NULL);
+  SDL_RenderPresent(ren);
+
+  SDL_Delay(2000);
 
   SDL_Quit();
   return 0;
